7aed0ffca8ecd3e1c1f539044017912ce840b3d0
Pavel <Pavel@MACs-MacBook-Pro.local>
2024-04-23 23:08:26 +0500

    edit app header

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,47 +1,27 @@
 import {BurgerIcon, ListIcon, Logo, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './app-header.module.css';
-import PropTypes from 'prop-types'
-
-const ButtonItem = (props)=> (
-    <div className ={`${styles.button} p-5 ml-2 mt-4 mb-4`}>
-        {props.children}
-    </div>
-);
-
-const ButtonsContainer = (props)=> (
-    <div className ={props.className}>
-        {props.children}
-    </div>
-);
-ButtonItem.propTypes = {
-    chuldren: PropTypes.element
-};
-ButtonsContainer.propTypes = {
-    className: PropTypes.string,
-    chuldren: PropTypes.element
-};
 
 export default function AppHeader(){
     return(
     <header className={styles.header}>
-        <ButtonsContainer className={styles.left}>
-            <ButtonItem>
+        <div className={styles.left}>
+            <div className ={`${styles.button} p-5 ml-2 mt-4 mb-4`}>
                 <BurgerIcon type="primary"/>
                 <p className="text text_type_main-default ml-2">Конструктор</p>
-            </ButtonItem>
-            <ButtonItem>
+            </div>
+            <div className ={`${styles.button} p-5 ml-2 mt-4 mb-4`}>
                 <ListIcon type="secondary"/>
                 <p className="text text_type_main-default text_color_inactive ml-2">Лента заказов</p>
-            </ButtonItem>
-        </ButtonsContainer>
-        <ButtonsContainer className={styles.middle}>
+            </div>
+        </div>
+        <div className={styles.middle}>
             <Logo/>
-        </ButtonsContainer>
-        <ButtonsContainer className={styles.right}>
-            <ButtonItem>
+        </div>
+        <div className={styles.right}>
+        <div className ={`${styles.button} p-5 ml-2 mt-4 mb-4`}>
                 <ProfileIcon type="secondary"/>
                 <p className="text text_type_main-default text_color_inactive ml-2">Личный кабинет</p>
-            </ButtonItem>
-        </ButtonsContainer>
+            </div>
+        </div>
       </header>)
 };
