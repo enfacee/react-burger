33892740a4929e4e2c903588a35a6b80b6645b6f
Pavel <enfac@mail.ru>
2024-05-30 23:13:46 +0500

    add routes

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,26 +1,15 @@
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
 import styles from './app.module.css';
 import AppHeader from '../app-header/app-header';
-import BurgerIngredients from '../burger-ingredients/burger-ingredients';
-import BurgerConstructor from '../burger-constructor/burger-constructor';
+import { Routes, Route } from 'react-router-dom';
+import { HomePage } from '../../pages/home';
 
-function App() {
+export default function App() {
   return (
     <div className={styles.app}>
       <AppHeader/>
-      <main className={`${styles.mainContainer} mb-8`}>
-        <DndProvider backend={HTML5Backend}>
-          <div className={styles.box}>
-            <BurgerIngredients/>
-          </div>
-          <div className={styles.box}>
-            <BurgerConstructor/>
-          </div>
-        </DndProvider>
-      </main>
-    </div>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+      </Routes>
+    </div>    
   )
-}
-
-export default App;
+};
